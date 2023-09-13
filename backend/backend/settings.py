90df8c48847ed9e31bb96fe2f629efc030099a7a
alexezpt <alexannmill@gmail.com>
2023-09-13 08:25:04 -0700

    Adding cors and allowed origins to application

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -27,6 +27,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+CORS_ORIGIN_WHITELIST = [
+     'http://localhost:3000'
+]
 
 # Application definition
 
@@ -48,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'backend.urls'
